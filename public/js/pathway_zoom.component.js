5d548bae6b3a385293bd915cc3866a31aa96f5ad
Nikko <dumriquenikko@gmail.com>
2020-06-28 14:17:59 -0700

    fixed the zooming implementation such that its more consitant with how far it zooms in

--- public/js/pathway_zoom.component.js
+++ public/js/pathway_zoom.component.js
@@ -21,34 +21,33 @@ AFRAME.registerComponent("pathway_zoom", {
         let gyroQuaternion = (new THREE.Quaternion()).copy(cameraGyro.GetQuaternion());
         let zoomIn = this.data.zoomPosition.applyQuaternion(gyroQuaternion);
 
-        console.log(cameraGyro.GetWorldPos())
-        this.MoveCameraRig(new THREE.Vector3(zoomIn.x, zoomIn.y, zoomIn.z + 1),new THREE.Vector3(0, .2, 0.25));
+        this.MoveCameraRig(new THREE.Vector3(zoomIn.x, zoomIn.y, zoomIn.z + 1),new THREE.Vector3(0, 0, Math.PI/2));
         cameraGyro.OnRemoveMouseDown();
 
         this.el.removeEventListener('click', this.ActivateZoomIn);
         this.el.addEventListener('click', this.ActivateZoomOut);
     },
+
     ActivateZoomOut: function(event) {
         console.log('zooming out');
         this.el.setAttribute('material', 'color', 'green');
         this.MoveCameraRig(new THREE.Vector3(1, -1.2, 5), new THREE.Vector3(0, 0, 0)); //todo: give this function the proper initial value
-        document.getElementById('gyro').components['drag-rotate-component'].OnAddMouseDown(); //not working
+        document.getElementById('gyro').components['drag-rotate-component'].OnAddMouseDown(); 
         this.el.removeEventListener('click', this.ActivateZoomOut);
         this.el.addEventListener('click', this.ActivateZoomIn);
     },
+
     MoveCameraRig: function(position, rotation) {
         let rotationDeg = new THREE.Vector3(rotation.x * (180 / Math.PI), rotation.y * (180 / Math.PI),rotation.z * (180 / Math.PI))
         document.getElementById('camera-rig').setAttribute('animation',{
             property: 'position',
             to: position.x + " " + position.y + " " + position.z,
-            dur: 1000,
             easing: 'linear',
             loop: false
         });
         document.getElementById('camera-rig').setAttribute('animation__2', {
             property: 'rotation',
             to: rotationDeg.x + " " + rotationDeg.y + " " + rotationDeg.z,
-            dur: 1000,
             easing: 'linear',
             loop: false
 
