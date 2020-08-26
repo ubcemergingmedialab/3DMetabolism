2eb58ba4c3e1a9bf1a41198b975d44fd7b5ca414
Nikko <dumriquenikko@gmail.com>
2020-08-26 10:57:46 -0700

    the pathways submenu is now dynamic. Still needs to be fully implemented

--- public/js/menu-presenter.js
+++ public/js/menu-presenter.js
@@ -1,52 +1,78 @@
 function menuPresenter() {
 
-
-    alert("hello world");
-    // var link = document.getElementsByClassName("Pathways");
-    // link.appendChild(document.createElement("BUTTON"));
-
-
     // var btn = document.createElement("BUTTON");   // Create a <button> element
     // btn.innerHTML = "CLICK ME";                   // Insert text
-    // link.body.appendChild(btn);   
+    // link.body.appendChild(btn);       
 
-    
+    function init() {
+        window.onload = function() {
+            private.initPathways();
+        }
+    }
 
-    // function init() {
-    //     console.log("made it here");
-    //     sequenceEventHandler();
-    //     console.log("also made it here");
-    //     }
+    var public = {
+        getHtml: function() {
+            initHtml();
+            this.getHtml()
+        },
 
-    // var public = {
-    //     getHtml: function() {
-    //         initHtml();
-    //         this.getHtml()
-    //     },
+        pathwaysEventHandler: function(pathway) {
+            
 
-    //     sequenceEventHandler: function() {
-    //         console.log("sequence called");
-    //     },
+        }
 
-    //     removeHtml: function() {}
-    // }
+    }
+    var private = {
+        updatePathways: function(pathway) {
+            try{ 
+                let iframe = document.getElementById("scene-iframe");
+                let target = iframe.contentWindow.document.getElementById("presenter");
 
-    // var __buttonTemplate = function(buttonText){
-    //     var btn = document.createElement("BUTTON");
-    //     btn.innerHTML(buttonText);
+                target.setAttribute("presenter", "activePathway:" + pathway);
+                console.log("setting pathway to: " + pathway);
+            } catch(e) {
+                console.log("pathway not set on assign-pathway component");
+            }
 
-    // }
+            try{
+                this.updateSequence(sequence);
+            } catch(e) {
+                console.log("cannot create pathway associated sequence menu");
+            }
+        },
+
+        updateSequence: function(sequence) {
+
+        },
+
+        initPathways: function() {
+            let pathwayClass = document.getElementsByClassName("Pathways")[0];
+            let btnAllPathways = document.createElement("button");
+            let btnGluconeogenesis = document.createElement("button");
+            let btnGlycolysis = document.createElement("button");
+
+            // btnAllPathways.innerHTML("All Pathways");
+            // btnGluconeogenesis.innerHTML("Gluconeogenesis");
+            // btnGlycolysis.innerHTML("Glycolysis");
+
+            pathwayClass.appendChild(btnAllPathways);
+            pathwayClass.appendChild(btnGluconeogenesis);
+            pathwayClass.appendChild(btnGlycolysis);
+
+        }
+
+
+    }
+    var __buttonTemplate = function(buttonText){
+        var btn = document.createElement("BUTTON");
+        btn.innerHTML(buttonText);
+
+    }
 
     //TODO: initHtml();
     //TODO: getHtml();
     //TODO: sequenceEventHandler();
     //TODO: removeHtml();
 
-    // class SequenceMenu {
-    //     constructor(sequence) {
-    //         this.sequence = sequence;
-    //     }
-
-    // }
-    // init();
+    init();
 }
