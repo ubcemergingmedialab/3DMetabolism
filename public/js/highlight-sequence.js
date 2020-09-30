55b348038cab5f62a25fe18caa394452bc4587bd
Hai Lin Zhang <hailin.zhang@fatiguescience.com>
2020-09-30 14:16:52 -0700

    Add ability to highlight edges

--- public/js/highlight-sequence.js
+++ public/js/highlight-sequence.js
@@ -5,29 +5,48 @@ AFRAME.registerComponent("highlight-sequence", {
   init: function () {
     console.log("HIGHLIGHT INIT");
     this.el.addEventListener('click', () => {
-      let component = this.el.getAttribute("highlight-sequence");
-      let sequenceName = component.sequence;
-      for (let i = 0; i < View.sequences[sequenceName].length; i++) {
-        curSequence = View.sequences[sequenceName];
-        let metabolite = curSequence[i]
+      const component = this.el.getAttribute("highlight-sequence");
+      const sequenceName = component.sequence;
+      const nodes = View.sequences.nodes[sequenceName];
+      const edges = View.sequences.edges[sequenceName];
+      for (let i = 0; i < nodes.length; i++) {
+        const metabolite = nodes[i];
         console.log("HIGHLIGHT" + metabolite);
-        let curElement = document.getElementById(metabolite);
+        const curElement = document.getElementById(metabolite);
         if (curElement === null) {
           console.log("could not find node " + metabolite);
           continue;
         }
         curElement.setAttribute("material", "color", "red");
-        if (curSequence[i + 1] != null) {
-          let input = curSequence[i];
-          let output = curSequence[i + 1];
-          console.log("looking for " + input + "/" + output);
-          let curEdge = document.getElementById(input + "/" + output);
+        if (nodes[i + 1] !== null) {
+          const outputMetabolite = nodes[i + 1];
+          console.log("looking for " + metabolite + "/" + outputMetabolite);
+          const curEdge = document.getElementById(metabolite + "/" + outputMetabolite);
           if (curEdge === null) {
-            console.log("could not find edge " + input + "/" + output);
+            console.log("could not find edge " + metabolite + "/" + outputMetabolite);
             continue;
           }
           curEdge.setAttribute("material", "color", "purple");
         }
+      }
+      for (let i = 0; i < edges.length; i++) {
+        const edge = edges[i];
+        const edgeName = edge.input + '/' + edge.output;
+        console.log("HIGHLIGHT" + edgeName);
+        const curEdge = document.getElementById(edgeName);
+        if (curEdge === null) {
+          console.log("could not find node " + edgeName);
+          continue;
+        }
+        curEdge.setAttribute("material", "color", "purple");
+        [edge.input, edge.output].forEach((metabolite) => {
+          const curElement = document.getElementById(metabolite);
+          if (curElement === null) {
+            console.log("could not find node " + metabolite);
+          } else {
+            curElement.setAttribute("material", "color", "red");
+          }
+        });
       }
     });
   }
