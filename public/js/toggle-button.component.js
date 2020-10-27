47dc414973d3a5b80a461f4b7bff907b3f1d4dd5
Nikko <dumriquenikko@gmail.com>
2020-10-27 16:40:01 -0700

    Implemented the animate and stopAnimation functions

--- public/js/toggle-button.component.js
+++ public/js/toggle-button.component.js
@@ -10,6 +10,14 @@ AFRAME.registerComponent('toggledropdown', {
               if (!this.el.getAttribute('gui-icon-button').toggle) {
                   target.dispatchEvent(new Event('closedropdown', {'bubbles': true}));
                   this.el.getAttribute('gui-icon-button').toggle = true;
+                  try {
+                    let testEdge = document.getElementById('glucose_6_phosphate/glucose_1_phosphate')
+                    console.log("calling animate");
+                    testEdge.setAttribute('material','active',1.0);
+                } catch (error) {
+                    console.log('could not start animation of edge:' + this.el.id);
+                }
+            
               }
               else {
                 target.dispatchEvent(new Event('opendropdown', {'bubbles': true}));
