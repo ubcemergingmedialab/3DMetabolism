5d90c1ee98348962dd427cd576c5a87a1fe116b1
Nikko <dumriquenikko@gmail.com>
2020-11-12 16:16:52 -0800

    implemented most of the funtionality of the menu. Still need to add buttons for animation and organize the menu style

--- public/js/UIPresenter.js
+++ public/js/UIPresenter.js
@@ -6,37 +6,109 @@ UIPresenter = function(pathway, sequence) {
     //private
     function initMenu() {
         this.entity = document.createElement('a-entity');
+        this.entity.setAttribute('opacity','1')
         this.entity.setAttribute('position', '-6 8 2');
-        const flexContainer = createFlexContainer();
+        const flexContainer = createFlexContainers();
         this.entity.appendChild(flexContainer);
         this.scene.appendChild(this.entity)
     }
 
+
     //private
-    function attachButtons(flexContainer) {
+    function createFlexContainers() {
+        const parentFlexContainter = document.createElement('a-gui-flex-container');
+        const childHighlightFlexContainer = document.createElement('a-gui-flex-container');
+        const childAnimationsFlexContainer = document.createElement('a-gui-flex-container');
+        const childToggleLabelFlexContainer = document.createElement('a-gui-flex-container');
+
+        const parentColumnContainer = document.createElement('a-gui-flex-container');
+        parentColumnContainer.setAttribute('opacity','0.5')
+        parentColumnContainer.setAttribute('flex-direction','row')
+
+        parentFlexContainter.appendChild(parentColumnContainer);
+        parentColumnContainer.appendChild(childHighlightFlexContainer);
+        parentColumnContainer.appendChild(childAnimationsFlexContainer)
+        childHighlightFlexContainer.appendChild(childToggleLabelFlexContainer)
+        // parentFlexContainter.appendChild(childToggleLabelFlexContainer);
+        // parentFlexContainter.appendChild(childAnimationsFlexContainer);
+        childAnimationsFlexContainer.append(createAnimationButton())
+        // parentFlexContainter.setAttribute('flex-direction','row');
+        childToggleLabelFlexContainer.appendChild(createLabelToggleButton())
+
+        parentFlexContainter.setAttribute('flex-direction','row')
+        parentFlexContainter.setAttribute('position','15 -5.5 0')
+
+        parentColumnContainer.setAttribute('align-items','normal')
+        parentColumnContainer.setAttribute('justify-content','center')
+
+        childToggleLabelFlexContainer.setAttribute('flex-direction','column')
+        childToggleLabelFlexContainer.setAttribute('align-items','normal')
+        childToggleLabelFlexContainer.setAttribute('justify-content','center')
+        childToggleLabelFlexContainer.setAttribute('opacity','0')
+        childToggleLabelFlexContainer.setAttribute('width','3')
+        childToggleLabelFlexContainer.setAttribute('height','1')
+
+        childAnimationsFlexContainer.setAttribute('flex-direction','column');
+        childAnimationsFlexContainer.setAttribute('justify-content','center')
+        childAnimationsFlexContainer.setAttribute('align-items','normal');
+        childAnimationsFlexContainer.setAttribute('component-padding','0.1');
+        childAnimationsFlexContainer.setAttribute('opacity','0');
+        childAnimationsFlexContainer.setAttribute('width','0.75');
+        childAnimationsFlexContainer.setAttribute('height','5');
+        childAnimationsFlexContainer.setAttribute('position','0.5 0 0');
+        childAnimationsFlexContainer.setAttribute('visible', 'true');
+
+        childHighlightFlexContainer.setAttribute('flex-direction','column');
+        childHighlightFlexContainer.setAttribute('visible','true');
+        childHighlightFlexContainer.setAttribute('opacity','0.2');
+        childHighlightFlexContainer.setAttribute('width','4.5');
+        childHighlightFlexContainer.setAttribute('height', '6.5');
+        childHighlightFlexContainer.setAttribute('justify-content','center')
+        childHighlightFlexContainer.setAttribute('align-items','normal')
+
+        attachButtons(childHighlightFlexContainer, createHighlightButton)
+        attachButtons(childAnimationsFlexContainer, createAnimationButton)
+        return parentFlexContainter;
+    }
+
+
+    //private
+    function attachButtons(flexContainer, buttonType) {
         for(var key in this.pathway) {
-            const button = createHighlightButton(key)
+            const button = buttonType(key)
             flexContainer.appendChild(button)
         }
     }
 
-    //private
-    function createFlexContainer() {
-        const menuFlexContainter = document.createElement('a-gui-flex-container')
-        const highlightFlexContainer = document.createElement('a-gui-flex-container')
-        menuFlexContainter.setAttribute('flex-direction','row');
-        highlightFlexContainer.setAttribute('flex-direction','column');
-        highlightFlexContainer.setAttribute('visible','true');
-        highlightFlexContainer.setAttribute('opacity','0.2');
-        highlightFlexContainer.setAttribute('width','4.5');
-        highlightFlexContainer.setAttribute('height', '6.5');
-        highlightFlexContainer.setAttribute('justify-content','center')
-        highlightFlexContainer.setAttribute('align-items','normal')
-        attachButtons(highlightFlexContainer)
-        menuFlexContainter.appendChild(highlightFlexContainer);
-        return menuFlexContainter;
+    function createLabelToggleButton() {
+        const button = document.createElement('a-gui-toggle');
+        button.setAttribute('position','0 0 0')
+        button.setAttribute('class','interactible');
+        button.setAttribute('width','2.5')
+        button.setAttribute('height','0.75');
+        button.setAttribute('onclick', 'toggleLabel')
+        button.setAttribute('value','hide labels')
+        button.setAttribute('font-family','Arial')
+        button.setAttribute('margin','0 0 0.05 0')
+        button.setAttribute('id','label-toggle')
+        return button
     }
 
+    function createAnimationButton(pathway, sequence) {
+        const button = document.createElement('a-gui-button');
+        button.setAttribute('height','0.5')
+        button.setAttribute('width','0.5')
+        button.setAttribute('class','interactible')
+        button.setAttribute('active-color','#d1d1d1')
+        button.setAttribute('background-color','#d1d1d1')
+        button.setAttribute('hover-color','#d1d1d1')
+        button.setAttribute('margin','0 0 0.05 0')
+        button.setAttribute('icon','chevron-right')
+        button.setAttribute('visible','true')
+        button.setAttribute('toggle','true')
+        return button
+
+    }
     //private
     function createHighlightButton(pathway) {
         const button = document.createElement('a-gui-button');
