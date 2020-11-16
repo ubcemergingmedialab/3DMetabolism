dc1575ca4770944029b6b43fbbba4c7911a4e14f
Dante <dcerron1@mail.ubc.ca>
2020-11-16 11:04:24 -0800

    implemented new shader animation

--- public/js/sequence-animation.component.js
+++ public/js/sequence-animation.component.js
@@ -1,32 +1,32 @@
 AFRAME.registerComponent("sequence-animation", {
     schema: {
-        activeSequence: {type:"string", default: "1"}
+        activeSequence: { type: "string", default: "1" }
     },
 
-    init: function() {
+    init: function () {
         var data = this.data;
         this.activateNextAnimation = this.activateNextAnimation.bind(this);
         this.findNextMolecule = this.findNextMolecule.bind(this);
         this.findNextMoleculeRandom = this.findNextMoleculeRandom.bind(this);
         this.getFirstMolecule = this.getFirstMolecule.bind(this);
 
-        setInterval(() => {
-            if(data.activeSequence == "random") {
-                console.log("calling random");
-                return;
-            } else {
-                let firstMolecule = this.getFirstMolecule(data.activeSequence);
-                if(firstMolecule == null) {
-                    console.log("could not find molecule in sequence " + data.activeSequence);
-                    return;
-                }
-                this.activateNextAnimation(firstMolecule, this.findNextMolecule(firstMolecule, data.activeSequence), 1000, data.activeSequence);
-            }
-        }, 2000)
+        /*     setInterval(() => {
+                 if (data.activeSequence == "random") {
+                     console.log("calling random");
+                     return;
+                 } else {
+                     let firstMolecule = this.getFirstMolecule(data.activeSequence);
+                     if (firstMolecule == null) {
+                         console.log("could not find molecule in sequence " + data.activeSequence);
+                         return;
+                     }
+                     this.activateNextAnimation(firstMolecule, this.findNextMolecule(firstMolecule, data.activeSequence), 1000, data.activeSequence);
+                 }
+             }, 2000)*/
         document.addEventListener('keydown', (e) => {
             console.log("keydown");
-            switch(e.keyCode) {
-                case 49: 
+            switch (e.keyCode) {
+                case 49:
                     this.el.setAttribute("sequence-animation", "activeSequence", "1");
                     break;
                 case 50:
@@ -35,7 +35,7 @@ AFRAME.registerComponent("sequence-animation", {
                     break;
                 case 32:
                     let firstMolecule = this.getFirstMolecule(data.activeSequence);
-                    if(firstMolecule == null) {
+                    if (firstMolecule == null) {
                         console.log("could not find molecule in sequence " + data.activeSequence);
                         break;
                     }
@@ -49,9 +49,32 @@ AFRAME.registerComponent("sequence-animation", {
     //@param2: element on which this animation will end
     //@param3: how long the animation will go for
     //@param4: the sequence this animation is a part of (if null, decide randomly)
-    activateNextAnimation: function(start, end, time, sequence) {
+    activateNextAnimation: function (start, end, time, sequence) {
         let startObject = start.object3D;
         let endObject = end.object3D;
+        let edge = Model.fetchEdge(start.getAttribute("id"), end.getAttribute("id"));
+        if (edge) {
+            if (!edge.components["animation-behavior"]) {
+                edge.setAttribute("animation-behavior", "");
+            }
+            edge.components['animation-behavior'].callAnimate();
+        }
+
+        let startColor, endColor;
+        startColor = start.getAttribute("material").color;
+        start.setAttribute("material", { color: "#fff" });
+        endColor = end.getAttribute("material").color;
+        start.setAttribute("animation__" + this.data.activeSequence, {
+            property: 'material.color',
+            dur: 1000,
+            to: startColor
+        })
+        end.setAttribute("animation__" + this.data.activeSequence, {
+            property: 'material.color',
+            dur: 1000,
+            to: "#fff"
+        })
+        /*
         let markerEl = document.createElement("a-entity");
         markerEl.setAttribute("geometry", {
             primitive: 'sphere',
@@ -72,44 +95,54 @@ AFRAME.registerComponent("sequence-animation", {
             easing: 'easeInQuad'
         });
         document.getElementById("sceneModel").appendChild(markerEl);
+        */
         setTimeout(() => {
             let curStart = end;
             let curEnd;
             //if sequence is null, go with random behaviour, otherwise go by sequence list
-            if(sequence == null) {
+            if (sequence == null) {
                 curEnd = this.findNextMoleculeRandom(end)
             } else {
                 curEnd = this.findNextMolecule(end, sequence);
             }
-            if(curEnd != null) {
+            if (curEnd != null) {
                 this.activateNextAnimation(curStart, curEnd, time, sequence);
             }
-            markerEl.remove();
+            if (edge) {
+                edge.components['animation-behavior'].callStopAnimation();
+            }
+
+            end.setAttribute("animation__" + this.data.activeSequence + "end", {
+                property: 'material.color',
+                dur: 1000,
+                to: endColor
+            })
+            //markerEl.remove();
         }, time);
     },
 
     //@param1 the name of the active sequence to get first molecule from
     getFirstMolecule(sequence) {
-        return document.getElementById(View.nodes[View.sequences[sequence][0]].name);
+        return document.getElementById(Model.sequences.nodes[sequence][0]);
     },
 
     //@param1 element for which we want to find the next molecule
     //@param2 sequence within which to search for next molecule
     findNextMolecule(current, sequence) {
-        let currentSequence = View.sequences[sequence];
+        let currentSequence = Model.sequences.nodes[sequence];
         let currentName = current.getAttribute("id");
         let foundElement = false;
-        for(let element of currentSequence) {
-            if(foundElement == true) {
+        for (let element of currentSequence) {
+            if (foundElement == true) {
                 return document.getElementById(element);
             }
-            if(element == currentName) {
+            if (element == currentName) {
                 foundElement = true;
             }
         }
     },
 
-    findNextMoleculeRandom: function(current) {
+    findNextMoleculeRandom: function (current) {
         console.log('called random');
         return;
     }
