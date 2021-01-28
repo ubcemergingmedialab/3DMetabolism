de53260ad2b3bf946ca9a25413ffc6a9a9b3563c
Hailin Zhang <hzhan3369@gmail.com>
2021-01-28 01:46:38 -0800

    Add mockup of buttons

--- public/js/pathway-menu.component.js
+++ public/js/pathway-menu.component.js
@@ -29,23 +29,40 @@ AFRAME.registerComponent("pathway-menu", {
     parentContainer.setAttribute("flex-direction", "column");
     parentContainer.setAttribute("class", "interactible");
     parentContainer.setAttribute("opacity", "0.5");
-    parentContainer.setAttribute("class", "pathwayMenu")
-    
+    parentContainer.setAttribute("class", "pathwayMenu");
+
     contentContainer.setAttribute("flex-direction", "column");
 
-    const titleElem = document.createElement('a-gui-label');
-    titleElem.setAttribute('value', this.data.pathwayName);
-    titleElem.setAttribute('height', 0.1);
-    titleElem.setAttribute('font-size', '42px');
-    titleElem.setAttribute('opacity', 0.9);
+    const titleElem = document.createElement("a-gui-label");
+    titleElem.setAttribute("value", this.data.pathwayName);
+    titleElem.setAttribute("height", 0.1);
+    titleElem.setAttribute("font-size", "42px");
+    titleElem.setAttribute("opacity", 0.9);
 
-    const descriptionElem = document.createElement('a-gui-label');
-    descriptionElem.setAttribute('value', 'a description about the pathway');
-    descriptionElem.setAttribute('font-size', '30px');
-    descriptionElem.setAttribute('opacity', 0.9);
+    const descriptionElem = document.createElement("a-gui-label");
+    const nodes = Model.sequences.nodes[this.data.pathwayName];
+    if (Object.keys(nodes).length > 0) {
+      descriptionElem.setAttribute("height", "0.7");
+    }
+    descriptionElem.setAttribute("value", "a description about the pathway");
+    descriptionElem.setAttribute("font-size", "30px");
+    descriptionElem.setAttribute("opacity", 0.9);
 
     contentContainer.appendChild(titleElem);
     contentContainer.appendChild(descriptionElem);
+
+    if (nodes) {
+      Array.from(Object.keys(nodes)).forEach((sequence) => {
+        const buttonElem = document.createElement("a-gui-button");
+        buttonElem.setAttribute("font-size", "32px");
+        buttonElem.setAttribute("animation-button-behavior", `sequence:${sequence}`);
+        buttonElem.setAttribute("height", "0.1");
+        buttonElem.setAttribute("class", "interactible");
+        buttonElem.setAttribute("value", sequence);
+        contentContainer.appendChild(buttonElem);
+      });
+    }
+
     parentContainer.appendChild(contentContainer);
 
     return parentContainer;
